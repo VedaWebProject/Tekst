b37cdff696926bb77f548b0b4f99701678ba3171
bkis <kiss@mailbox.org>
2022-03-24 16:56:28 +0100

    Fix how tests are importing the project package

--- server/tests/test_dummy.py
+++ server/tests/test_dummy.py
@@ -1,5 +1,6 @@
 import pytest
-from .context import textrig
+
+from textrig import app
 
 
 def test_holy_hand_grenade_of_antioch():
@@ -7,3 +8,8 @@ def test_holy_hand_grenade_of_antioch():
     for i in range(0, 3):
         count += 1
     assert count == 3, "Three shalt be the number thou shalt count."
+
+
+def test_app_ref():
+    app.run()
+    assert True
