1d0303d0c4161f0980fc11ae6aa3184dd00eba6e
bkis <kiss@mailbox.org>
2022-06-01 16:14:24 +0200

    Update project structure

--- server/src/app.py
+++
@@ -1,7 +0,0 @@
-def run():
-    print("This is TextRig...")
-    x = 4
-    if x == 42:
-        print(x)
-    else:
-        print(f"{x} is not 42.")
---
+++ server/src/textrig/app.py
@@ -0,0 +1,10 @@
+from textrig import __VERSION__
+
+
+def run():
+    print(f"This is TextRig v{__VERSION__}...")
+    x = 4
+    if x == 42:
+        print(x)
+    else:
+        print(f"{x} is not 42.")
