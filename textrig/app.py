7a0295e5f3b7510cf975d55a867a57890245082d
bkis <kiss@mailbox.org>
2022-07-18 17:38:18 +0200

    Init server repo

---
+++ textrig/app.py
@@ -0,0 +1,10 @@
+from textrig import __version__
+
+
+def run():
+    print(f"This is TextRig v{__version__}...")
+    x = 4
+    if x == 42:
+        print(x)
+    else:
+        print(f"{x} is not 42.")
