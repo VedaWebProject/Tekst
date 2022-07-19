5941e814ef3a0ecb52cb522837659c18fc4ca372
bkis <kiss@mailbox.org>
2022-07-19 12:11:54 +0200

    Clean up dummy test cases

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,12 +1,5 @@
-from textrig import __version__, main
-
-
 def test_holy_hand_grenade_of_antioch():
     count = 0
     for i in range(0, 3):
         count += 1
     assert count == 3, "Three shalt be the number thou shalt count."
-
-
-def test_app_ref():
-    assert main.hello_world()["version"] == __version__
--- tests/test_textrig.py
+++ tests/test_textrig.py
@@ -1,5 +1,9 @@
-from textrig import __version__
+from textrig import __version__, main
 
 
 def test_version():
     assert __version__ == "0.0.1"
+
+
+def test_hello_world():
+    assert main.hello_world()["version"] == __version__
