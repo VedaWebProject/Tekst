b79a2e0b27a933088bbe10050f1428ed3f5978a5
bkis <kiss@mailbox.org>
2022-08-02 11:34:20 +0200

    Update dynamic config logic

--- tests/test_api.py
+++ tests/test_api.py
@@ -11,4 +11,4 @@ def test_config(client):
     response = client.get("/config")
     assert response.status_code == 200
     assert response.json()["app_name"] == "TextRig Test Instance"
-    assert response.json()["db_uri"] == "mongodb://127.0.0.1:27017"
+    assert response.json()["db_uri"] == "mongodb://root:root@127.0.0.1:27017"
--- textrig/config.py
+++ textrig/config.py
@@ -1,4 +1,5 @@
 from functools import lru_cache
+from urllib.parse import quote as q
 
 from pydantic import BaseSettings, validator
 
@@ -8,13 +9,17 @@ class Config(BaseSettings):
     app_name: str = "TextRig"
     db_host: str = "127.0.0.1"
     db_port: str = "27017"
-    db_uri: str = f"mongodb://{db_host}:{db_port}"
     db_user: str = "root"
     db_pass: str = "root"
+    db_uri: str = f"mongodb://{db_user}:{q(db_pass.encode('utf8'))}@{db_host}:{db_port}"
 
-    @validator('db_uri', pre=True)
+    @validator("db_uri", pre=True)
     def val_db_uri(cls, _, values):
-        return f"mongodb://{values['db_host']}:{values['db_port']}"
+        return (
+            f"mongodb://{values['db_user']}:"
+            f"{q(values['db_pass'].encode('utf8'))}@"
+            f"{values['db_host']}:{values['db_port']}"
+        )
 
     class Config:
         env_file = ".env"
