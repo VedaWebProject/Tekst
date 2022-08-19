5100d7a0d653aae04ce5194b592735127b9533d4
bkis <kiss@mailbox.org>
2022-08-19 09:37:04 +0200

    Implement common data model bits

--- textrig/models/common.py
+++ textrig/models/common.py
@@ -1,18 +1,32 @@
+import datetime
+from typing import Optional
+
 from bson.objectid import ObjectId
-from pydantic import BaseModel, Field
+from pydantic import BaseModel as PydanticBaseModel
+from pydantic import Field, validator
+from pydantic.main import ModelMetaclass
 
 
-class ObjectIdStr(ObjectId):
-    """Wrapper model for MongoDB's object IDs"""
+def convert_datetime_to_realworld(dt: datetime.datetime) -> str:
+    return dt.replace(tzinfo=datetime.timezone.utc).isoformat().replace("+00:00", "Z")
 
+
+def convert_field_to_camel_case(string: str) -> str:
+    return "".join(
+        word if index == 0 else word.capitalize()
+        for index, word in enumerate(string.split("_"))
+    )
+
+
+class PyObjectId(ObjectId):
     @classmethod
     def __get_validators__(cls):
         yield cls.validate
 
     @classmethod
-    def validate(cls, v):
+    def validate(cls, v) -> ObjectId:
         if not ObjectId.is_valid(v):
-            raise ValueError(f"Invalid ObjectId: {v}")
+            raise ValueError("Invalid objectid")
         return ObjectId(v)
 
     @classmethod
@@ -20,10 +34,44 @@ class ObjectIdStr(ObjectId):
         field_schema.update(type="string")
 
 
-class DbModel(BaseModel):
-    """Base model for all further TextRig data models"""
+class BaseModel(PydanticBaseModel):
+    class Config:
+        allow_population_by_field_name = True
+        json_encoders = {datetime.datetime: convert_datetime_to_realworld}
+        alias_generator = convert_field_to_camel_case
 
-    id: ObjectIdStr = Field(default_factory=ObjectIdStr, alias="_id")
+
+class DateTimeModelMixin(BaseModel):
+    created_at: datetime.datetime = None
+    updated_at: datetime.datetime = None
+
+    @validator("created_at", "updated_at", pre=True)
+    def default_datetime(cls, value: datetime.datetime) -> datetime.datetime:
+        return value or datetime.datetime.now()
+
+
+class IDModelMixin(BaseModel):
+    id: PyObjectId | None = Field(default_factory=PyObjectId, alias="_id")
 
     class Config:
-        json_encoders = {ObjectId: lambda x: str(x)}
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+
+
+class AllOptional(ModelMetaclass):
+    """
+    Metaclass to render all fields of a pydantic model optional.
+    This approach was taken from here:
+    https://stackoverflow.com/a/67733889/7399631
+    Alternative ones are discussed here:
+    https://github.com/pydantic/pydantic/issues/1223#issuecomment-998160737
+    """
+    def __new__(self, name, bases, namespaces, **kwargs):
+        annotations = namespaces.get("__annotations__", {})
+        for base in bases:
+            annotations.update(base.__annotations__)
+        for field in annotations:
+            if not field.startswith("__"):
+                annotations[field] = Optional[annotations[field]]
+        namespaces["__annotations__"] = annotations
+        return super().__new__(self, name, bases, namespaces, **kwargs)
