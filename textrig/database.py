6d0c53504b63a760b3b6f387f45d6e45832fceb8
bkis <kiss@mailbox.org>
2022-08-19 09:38:01 +0200

    Prepare database module

--- textrig/database.py
+++ textrig/database.py
@@ -1,9 +1,15 @@
-import motor.motor_asyncio as motor
+from motor.motor_asyncio import AsyncIOMotorClient as DatabaseClient
+from motor.motor_asyncio import AsyncIOMotorDatabase as Database
 from textrig.config import TextRigConfig, get_config
 
 
 _cfg: TextRigConfig = get_config()
 
 # init db connection
-_client = motor.AsyncIOMotorClient(_cfg.db.get_uri())
-db = _client[_cfg.db.db_name]
+_client: DatabaseClient = DatabaseClient(_cfg.db.get_uri())
+_db: Database = _client[_cfg.db.db_name]
+
+
+# database object getter for use as a dependency
+def get_db() -> Database:
+    return _db
