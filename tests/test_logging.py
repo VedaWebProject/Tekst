80b28de112e3b6dbaa3c224ee65c792363f0e554
bkis <kiss@mailbox.org>
2022-10-07 15:56:50 +0200

    Add logging setup test 🥴

---
+++ tests/test_logging.py
@@ -0,0 +1,15 @@
+from textrig import logging
+import os
+
+
+def test_logging_setup_without_errors(testing_config):
+    os.environ["DEV_MODE"] = "false"
+    logging.setup_logging()
+    logging.log.info("foo bar")
+    os.environ["DEV_MODE"] = "true"
+    logging.setup_logging()
+    logging.log.info("foo bar")
+    os.environ["SERVER_SOFTWARE"] = "gunicorn"
+    logging.setup_logging()
+    logging.log.info("foo bar")
+    assert True
