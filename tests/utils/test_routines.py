c582ec4ddaafa8847a919290e2346b10d46f350e
bkis <kiss@mailbox.org>
2022-10-07 17:16:13 +0200

    Prepare routines test

---
+++ tests/utils/test_routines.py
@@ -0,0 +1,5 @@
+from textrig.utils import routines
+
+
+def test_load_snippets():
+    routines.load_snippets()  # TODO
--- textrig/utils/routines.py
+++ textrig/utils/routines.py
@@ -4,5 +4,5 @@ from textrig.config import TextRigConfig, get_config
 _cfg: TextRigConfig = get_config()
 
 
-def load_snippets():
+def load_snippets() -> dict[str, dict]:
     pass
