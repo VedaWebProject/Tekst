e10886167201f49bab05ee0a573fa48b190854cd
bkis <kiss@mailbox.org>
2022-10-21 18:08:47 +0200

    Try and see if service containers for actions work

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,14 +8,11 @@ def test_uidata(config, test_client):
     assert response.json()["platform"]["version"] == pkg_meta["version"]
 
 
-# """
-# TODO: This test needs to have a testing DB in every possible test environment!
-# """
-# def test_create_text(config, test_client):
-#     endpoint = f"{config.root_path}/texts"
-#     payload = {"title": "Just a Test"}
-#     resp = test_client.post(endpoint, json=payload)
-#     assert resp.status_code == 201, f"RESP: {resp.status_code} ({resp.reason})"
-#     assert "id" in resp.json()
-#     assert "slug" in resp.json()
-#     assert resp.json()["slug"] == "justatest"
+def test_create_text(config, test_client):
+    endpoint = f"{config.root_path}/texts"
+    payload = {"title": "Just a Test"}
+    resp = test_client.post(endpoint, json=payload)
+    assert resp.status_code == 201, f"RESP: {resp.status_code} ({resp.reason})"
+    assert "id" in resp.json()
+    assert "slug" in resp.json()
+    assert resp.json()["slug"] == "justatest"
