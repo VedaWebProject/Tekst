45077ab9145526db8eb91164738396452b5cac82
bkis <kiss@mailbox.org>
2022-10-31 13:52:43 +0100

    Hold index definitions in factory method

--- textrig/db/indexes.py
+++ textrig/db/indexes.py
@@ -5,54 +5,50 @@ from textrig.config import TextRigConfig, get_config
 from textrig.dependencies import get_db, get_db_client
 
 
-_index_models = {
-    "texts": [
-        pymongo.IndexModel([("slug", pymongo.ASCENDING)], name="slug", unique=True)
-    ],
-    "nodes": [
-        pymongo.IndexModel(
-            [
-                ("textSlug", pymongo.ASCENDING),
-                ("level", pymongo.ASCENDING),
-                ("index", pymongo.ASCENDING),
-            ],
-            name="textSlug_level_index",
-        ),
-        pymongo.IndexModel(
-            [
-                ("textSlug", pymongo.ASCENDING),
-                ("parentId", pymongo.ASCENDING),
-            ],
-            name="textSlug_parentId",
-        ),
-    ],
-    "layers": [
-        pymongo.IndexModel(
-            [
-                ("textSlug", pymongo.ASCENDING),
-                ("level", pymongo.ASCENDING),
-                ("public", pymongo.ASCENDING),
-            ],
-            name="textSlug_level_public",
-        ),
-        pymongo.IndexModel(
-            [
-                ("textSlug", pymongo.ASCENDING),
-                ("ownerId", pymongo.ASCENDING),
-            ],
-            name="textSlug_ownerId",
-        ),
-    ],
-    "units": [
-        pymongo.IndexModel(
-            [
-                ("layerId", pymongo.ASCENDING),
-                ("nodeId", pymongo.ASCENDING),
-            ],
-            name="layerId_nodeId",
-        ),
-    ],
-}
+def _get_index_definitions() -> dict:
+    return {
+        "texts": [
+            pymongo.IndexModel([("slug", pymongo.ASCENDING)], name="slug", unique=True)
+        ],
+        "nodes": [
+            pymongo.IndexModel(
+                [
+                    ("textSlug", pymongo.ASCENDING),
+                    ("parentId", pymongo.ASCENDING),
+                    ("level", pymongo.ASCENDING),
+                    ("index", pymongo.ASCENDING),
+                ],
+                name="textSlug_parentId_level_index",
+                unique=True
+            ),
+        ],
+        "layers": [
+            pymongo.IndexModel(
+                [
+                    ("textSlug", pymongo.ASCENDING),
+                    ("level", pymongo.ASCENDING),
+                    ("public", pymongo.ASCENDING),
+                ],
+                name="textSlug_level_public",
+            ),
+            pymongo.IndexModel(
+                [
+                    ("textSlug", pymongo.ASCENDING),
+                    ("ownerId", pymongo.ASCENDING),
+                ],
+                name="textSlug_ownerId",
+            ),
+        ],
+        "units": [
+            pymongo.IndexModel(
+                [
+                    ("layerId", pymongo.ASCENDING),
+                    ("nodeId", pymongo.ASCENDING),
+                ],
+                name="layerId_nodeId",
+            ),
+        ],
+    }
 
 
 async def create_indexes(cfg: TextRigConfig = get_config()):
@@ -64,7 +60,8 @@ async def create_indexes(cfg: TextRigConfig = get_config()):
     """
 
     db: Database = get_db(get_db_client(cfg), cfg)
+    indexes = _get_index_definitions()
 
     # create indexes
-    for collection, indexes in _index_models.items():
+    for collection, indexes in indexes.items():
         await db[collection].create_indexes(indexes)
