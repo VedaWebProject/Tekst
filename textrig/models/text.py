ceff0a3e18522af0224229de32560ca3ee955e37
bkis <kiss@mailbox.org>
2022-10-31 14:09:42 +0100

    Constrain text node model

--- textrig/models/text.py
+++ textrig/models/text.py
@@ -17,8 +17,12 @@ class Node(BaseModel):
 
     text_slug: str = Field(..., description="Slug of the text this node belongs to")
     parent_id: PyObjectId | None = Field(None, description="ID of parent node")
-    level: int = Field(..., description="Index of structure level this node is on")
-    index: int = Field(..., description="Position among all text nodes on this level")
+    level: int = Field(
+        ..., description="Index of structure level this node is on", ge=0
+    )
+    index: int = Field(
+        ..., description="Position among all text nodes on this level", ge=0
+    )
     label: str = Field(..., description="Label for identifying this text node")
     meta: Metadata | None = Field(None, description="Arbitrary metadata")
 
