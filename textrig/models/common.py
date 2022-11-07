de43494446392b29a860a2eac56c67a70706f975
bkis <kiss@mailbox.org>
2022-11-07 16:41:02 +0100

    Allow model population by field name

--- textrig/models/common.py
+++ textrig/models/common.py
@@ -64,6 +64,7 @@ class BaseModel(PydanticBaseModel):
 
     class Config:
         alias_generator = snake_to_camel_case
+        allow_population_by_field_name = True
 
 
 class ObjectInDB(PydanticBaseModel):
