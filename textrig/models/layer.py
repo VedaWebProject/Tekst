d2e1cad9f694b75a86dee3e4a3c4c4ff16eda272
bkis <kiss@mailbox.org>
2022-11-10 12:30:26 +0100

    Layers should have a required title

--- textrig/models/layer.py
+++ textrig/models/layer.py
@@ -5,6 +5,9 @@ from textrig.models.common import AllOptional, DbDocument, Metadata, TextRigBase
 class Layer(TextRigBaseModel):
     """A data layer describing a set of data on a text"""
 
+    title: str = Field(
+        ..., min_length=1, max_length=64, description="Title of this layer"
+    )
     text_slug: str = Field(..., description="Slug of the text this layer belongs to")
     level: int = Field(..., description="Text level this layer belongs to")
     layer_type: str = Field(...)
