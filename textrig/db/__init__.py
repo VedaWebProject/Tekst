63ddcddffd2dcdb9aabd8c21943e384666d4882b
bkis <kiss@mailbox.org>
2022-11-15 10:35:05 +0100

    Add missing typing

--- textrig/db/__init__.py
+++ textrig/db/__init__.py
@@ -4,13 +4,13 @@ from motor.motor_asyncio import AsyncIOMotorClient as DatabaseClient
 _db_client: DatabaseClient = None
 
 
-def init_client(db_uri: str):
+def init_client(db_uri: str) -> None:
     global _db_client
     if _db_client is None:
         _db_client = DatabaseClient(db_uri)
 
 
-def get_client(db_uri: str):
+def get_client(db_uri: str) -> DatabaseClient:
     global _db_client
     init_client(db_uri)
     return _db_client
