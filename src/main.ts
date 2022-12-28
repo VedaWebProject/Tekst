efa65edea823b2c708c3a10b8dbafd9f54117041
bkis <kiss@mailbox.org>
2022-12-28 10:21:07 +0100

    Draft i18n

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { createI18n } from 'vue-i18n';
+import staticI18nMsgs from '@intlify/unplugin-vue-i18n/messages';
 
 import App from './App.vue';
 import router from './router';
 
 import './assets/main.css';
 
+const i18n = createI18n({
+  legacy: false,
+  globalInjection: true,
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages: staticI18nMsgs,
+});
+
 const app = createApp(App);
 
+app.use(i18n);
 app.use(createPinia());
 app.use(router);
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { fileURLToPath, URL } from 'node:url';
+import { resolve, dirname } from 'node:path';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import checker from 'vite-plugin-checker';
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,6 +17,9 @@ export default defineConfig({
         lintCommand:
           'eslint . --ext .vue,.js,.jsx,.cjs,.mjs,.ts,.tsx,.cts,.mts --fix --ignore-path .gitignore',
       },
+    }),
+    VueI18nPlugin({
+      include: resolve(dirname(fileURLToPath(import.meta.url)), './src/locales/**'),
     }),
   ],
   resolve: {
