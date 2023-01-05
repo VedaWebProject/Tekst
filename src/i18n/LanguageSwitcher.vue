52e1f25683f1bfb19a6750eb3d6c09338943907f
bkis <kiss@mailbox.org>
2023-01-05 18:40:50 +0100

    Use NaiveUI components

--- src/i18n/LanguageSwitcher.vue
+++ src/i18n/LanguageSwitcher.vue
@@ -1,21 +1,25 @@
 <script setup lang="ts">
 import { i18n, setI18nLanguage } from '@/i18n';
+import { NButton, NDropdown } from 'naive-ui';
+import CaretDownOutlined from '@vicons/antd/CaretDownOutlined';
+import { Icon } from '@vicons/utils';
 import { ref } from 'vue';
 
 const selectedLanguage = ref(i18n.global.locale);
+const options = i18n.global.availableLocales.map((l) => {
+  return { label: l.toUpperCase(), key: l };
+});
 </script>
 
 <template>
-  <select v-model="selectedLanguage" @change="setI18nLanguage(selectedLanguage)">
-    <option
-      v-for="lang in $i18n.availableLocales"
-      :value="lang"
-      :selected="lang === selectedLanguage"
-      :key="lang"
-    >
-      {{ lang }}
-    </option>
-  </select>
+  <n-dropdown trigger="click" :options="options" @select="setI18nLanguage">
+    <n-button>
+      {{ selectedLanguage.toUpperCase() }}
+      <Icon style="margin-left: 4px">
+        <CaretDownOutlined />
+      </Icon>
+    </n-button>
+  </n-dropdown>
 </template>
 
 <style scoped></style>
