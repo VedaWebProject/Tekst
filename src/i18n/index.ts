c6fb9d7891be4d0b0b6b22f64179ed96345bf96f
bkis <kiss@mailbox.org>
2023-01-05 12:07:00 +0100

    Fix locale switch timing

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -18,17 +18,24 @@ export async function setI18nLanguage(locale: I18nOptions['locale'] = i18n.globa
   // @ts-ignore
   const l = locale?.value ?? locale ?? i18n.global.locale.value;
   if (!l) return Promise.reject(`Invalid locale code: ${l}`);
-  // @ts-ignore
-  i18n.global.locale.value = l;
-  document.querySelector('html')?.setAttribute('lang', l);
-  // fetch server i18n data
-  return await fetch(`${import.meta.env.TEXTRIG_SERVER_API}/uidata/i18n?lang=${l}`)
-    .then((response) => {
-      if (!response.ok) throw new Error('foo');
-      return response;
-    })
-    .then((response) => response.json())
-    .then((data) => {
-      i18n.global.mergeLocaleMessage(l, data);
-    });
+
+  try {
+    // fetch server i18n data
+    await fetch(`${import.meta.env.TEXTRIG_SERVER_API}/uidata/i18n?lang=${l}`)
+      .then((response) => {
+        if (!response.ok) throw new Error('foo');
+        return response;
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        i18n.global.mergeLocaleMessage(l, data);
+      });
+  } catch (e) {
+    return Promise.reject('Error loading translations from server');
+  } finally {
+    // @ts-ignore
+    i18n.global.locale.value = l;
+    document.querySelector('html')?.setAttribute('lang', l);
+  }
+  return Promise.resolve();
 }
