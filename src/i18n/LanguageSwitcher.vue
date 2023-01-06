ccc1a9c041593c1ac437b76a93ac3bf465d2552a
bkis <kiss@mailbox.org>
2023-01-06 08:30:49 +0100

    Change icon pack

--- src/i18n/LanguageSwitcher.vue
+++ src/i18n/LanguageSwitcher.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
 import { i18n, setI18nLanguage } from '@/i18n';
-import { NButton, NDropdown } from 'naive-ui';
-import CaretDownOutlined from '@vicons/antd/CaretDownOutlined';
-import { Icon } from '@vicons/utils';
+import { NButton, NDropdown, NIcon } from 'naive-ui';
+import { LanguageOutlined as LangIcon } from '@vicons/material';
 import { ref } from 'vue';
 
 const selectedLanguage = ref(i18n.global.locale);
@@ -13,11 +12,11 @@ const options = i18n.global.availableLocales.map((l) => {
 
 <template>
   <n-dropdown trigger="click" :options="options" @select="setI18nLanguage">
-    <n-button>
+    <n-button strong secondary type="primary" size="small" icon-placement="left">
+      <template #icon>
+        <n-icon :component="LangIcon" />
+      </template>
       {{ selectedLanguage.toUpperCase() }}
-      <Icon style="margin-left: 4px">
-        <CaretDownOutlined />
-      </Icon>
     </n-button>
   </n-dropdown>
 </template>
