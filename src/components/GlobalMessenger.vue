ab72ae9481e08d1ce2b5f107423585af13f531f8
bkis <kiss@mailbox.org>
2023-01-10 08:17:22 +0100

    Render messages as message alerts

--- src/components/GlobalMessenger.vue
+++ src/components/GlobalMessenger.vue
@@ -1,7 +1,38 @@
 <script setup lang="ts">
-import { defineComponent } from 'vue';
-import { NMessageProvider, useMessage } from 'naive-ui';
+import { defineComponent, h } from 'vue';
+import {
+  NMessageProvider,
+  useMessage,
+  NAlert,
+  useThemeVars,
+  type MessageRenderMessage,
+} from 'naive-ui';
 import { useMessagesStore } from '@/stores/messages';
+
+const themeVars = useThemeVars();
+
+const renderMessage: MessageRenderMessage = (props) => {
+  const { type } = props;
+  return h(
+    NAlert,
+    {
+      closable: props.closable,
+      onClose: props.onClose,
+      type: type === 'loading' ? 'default' : type,
+      // title: type in ['loading', 'default'] ? '' : type.toUpperCase(),
+      showIcon: true,
+      style: {
+        backgroundColor: themeVars.value.bodyColor,
+        boxShadow: 'var(--n-box-shadow)',
+        maxWidth: 'calc(100vw - 32px)',
+        width: '480px',
+      },
+    },
+    {
+      default: () => props.content,
+    }
+  );
+};
 
 const MessageDispatcher = defineComponent({
   setup() {
@@ -10,7 +41,12 @@ const MessageDispatcher = defineComponent({
 
     messages.$onAction(({ args }) => {
       const msg = args?.length && args[0];
-      msg && messageUtil.create(msg.text, { type: msg.type ?? 'default' });
+      msg &&
+        messageUtil.create(msg.text, {
+          type: msg.type ?? 'default',
+          render: renderMessage,
+          closable: true,
+        });
     });
 
     return () => null; // nothing to render
@@ -19,7 +55,13 @@ const MessageDispatcher = defineComponent({
 </script>
 
 <template>
-  <n-message-provider :duration="8000" placement="bottom" :max="5" closable keep-alive-on-hover>
+  <n-message-provider :duration="500000" placement="bottom" :max="5" keep-alive-on-hover closable>
     <MessageDispatcher />
   </n-message-provider>
 </template>
+
+<style>
+.n-alert__close {
+  z-index: 9999999;
+}
+</style>
