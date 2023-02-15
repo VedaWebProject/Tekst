a9620d1cc8eb6bb0a52ae0cffbb74ccf9e8130a6
bkis <kiss@mailbox.org>
2023-02-15 16:28:53 +0100

    Draft dummy account page

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
-import { useUiDataStore } from '@/stores/uiData';
+import { useUiDataStore, useAuthStore } from '@/stores';
+
 const ui = useUiDataStore();
+const auth = useAuthStore();
+const { email, firstName, lastName } = { ...auth.user };
 </script>
 
 <template>
-  <h1>Your {{ ui.get('platform.title') }} Account</h1>
-  <p>...</p>
+  <h1>
+    {{ firstName }}'{{ !firstName?.toLowerCase().endsWith('s') && 's' }}
+    {{ ui.get('platform.title') }} Account
+  </h1>
+  <ul>
+    <li><b>Email:</b> {{ email }}</li>
+    <li><b>Name:</b> {{ firstName }} {{ lastName }}</li>
+  </ul>
 </template>
