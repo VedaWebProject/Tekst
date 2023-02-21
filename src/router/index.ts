0d8ddbbffae88d5c13d969fe23c14f09ed90abda
bkis <kiss@mailbox.org>
2023-02-21 14:58:06 +0100

    Improve restricted routes logic

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,10 +3,6 @@ import { useAuthStore, useMessagesStore } from '@/stores';
 import { HomeView, AboutView, AccountView, LoginView, RegisterView } from '@/views';
 import { i18n } from '@/i18n';
 
-// restricted routes
-const ONLY_USERS = ['/account'];
-const ONLY_SUPERUSERS = ['/admin'];
-
 const t = i18n.global.t;
 
 const router = createRouter({
@@ -37,31 +33,34 @@ const router = createRouter({
       path: '/account',
       name: 'account',
       component: AccountView,
+      meta: { restricted: 'user' },
     },
     {
       path: '/admin',
       name: 'admin',
       component: HomeView,
+      meta: { restricted: 'superuser' },
     },
     { path: '/:pathMatch(.*)*', redirect: '/' },
   ],
 });
 
 router.beforeEach(async (to) => {
-  const auth = useAuthStore();
-  const messages = useMessagesStore();
-  // assess auth situation
-  const ru = ONLY_USERS.includes(to.path); // route is restricted to users
-  const rsu = ONLY_SUPERUSERS.includes(to.path); // route is restricted to to superusers
-  const l = auth.loggedIn; // a user is logged in
-  const u = auth.user?.isActive; // the user is a verified, active user
-  const su = auth.user?.isSuperuser; // the user is a superuser
-  const authorized = !(ru || rsu) || (ru && l && u) || (rsu && l && su);
-  // redirect if trying to access a restricted page without aithorization
-  if (!authorized) {
-    auth.returnUrl = to.fullPath;
-    messages.warning(t('errors.noAccess', { resource: to.path }));
-    return '/home';
+  if (to.meta?.restricted) {
+    const auth = useAuthStore();
+    const messages = useMessagesStore();
+    const ru = to.meta.restricted === 'user'; // route is restricted to users
+    const rsu = to.meta.restricted === 'superuser'; // route is restricted to to superusers
+    const l = auth.loggedIn; // a user is logged in
+    const u = auth.user?.isActive && auth.user?.isVerified; // the user is a verified, active user
+    const su = auth.user?.isSuperuser; // the user is a superuser
+    const authorized = (ru && l && u) || (rsu && l && su);
+    // redirect if trying to access a restricted page without aithorization
+    if (!authorized) {
+      auth.returnUrl = to.fullPath;
+      messages.warning(t('errors.noAccess', { resource: to.path }));
+      return '/home';
+    }
   }
 });
 
