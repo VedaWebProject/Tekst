a0ce0fe1e9f348651ad194d8deaa3ef58bb03d5d
bkis <kiss@mailbox.org>
2023-03-05 22:17:44 +0100

    Draft unit display components

---
+++ src/components/units/PlaintextUnit.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import type { PlainTextUnitRead } from '@/openapi';
+import UnitContainer from './UnitContainer.vue';
+const props = defineProps<PlainTextUnitRead>();
+</script>
+
+<template>
+  <UnitContainer :layer-title="props.layerId">
+    <div>{{ props.text }}</div>
+  </UnitContainer>
+</template>
---
+++ src/components/units/UnitContainer.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps<{
+  layerTitle?: string;
+}>();
+</script>
+
+<template>
+  <div class="content-block">
+    <h3>{{ props.layerTitle }}</h3>
+    <slot></slot>
+  </div>
+</template>
