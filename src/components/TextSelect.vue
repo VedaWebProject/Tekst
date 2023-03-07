715fb0da68fbd79917352ebc7540767a8e45f5bf
bkis <kiss@mailbox.org>
2023-03-07 09:05:29 +0100

    Style text selection dropdown

--- src/components/TextSelect.vue
+++ src/components/TextSelect.vue
@@ -1,23 +1,27 @@
 <script setup lang="ts">
 import type { TextRead } from '@/openapi';
-import { computed } from 'vue';
+import { computed, h } from 'vue';
 import { useStateStore, usePlatformStore } from '@/stores';
 import { useRouter } from 'vue-router';
 import { NDropdown, NButton, NIcon } from 'naive-ui';
 import ArrowDropDownFilled from '@vicons/material/ArrowDropDownFilled';
+import TextSelectOption from './TextSelectOption.vue';
 
 const router = useRouter();
 const state = useStateStore();
 const pf = usePlatformStore();
+const availableTexts = pf.data?.texts || [];
 
-const availableTexts = pf.data?.texts;
+function renderLabel(title: string, subtitle?: string, accentColor?: string) {
+  return h(TextSelectOption, { title, subtitle, accentColor });
+}
+
 const options = computed(() =>
-  availableTexts
-    ?.filter((t: TextRead) => t.id !== state.text?.id)
-    .map((t: TextRead) => ({
-      label: t.title,
-      key: t.slug,
-    }))
+  availableTexts.map((t: TextRead) => ({
+    label: () => renderLabel(t.title, t.subtitle, t.accentColor),
+    key: t.slug,
+    disabled: t.id === state.text?.id,
+  }))
 );
 
 function handleSelect(key: string) {
@@ -27,7 +31,7 @@ function handleSelect(key: string) {
       params: { ...router.currentRoute.value.params, text: key },
     });
   } else {
-    state.text = availableTexts?.find((t) => t.slug === key);
+    state.text = availableTexts.find((t) => t.slug === key);
   }
 }
 </script>
@@ -56,3 +60,11 @@ function handleSelect(key: string) {
     </n-button>
   </n-dropdown>
 </template>
+
+<style scoped>
+.text-select-option-indicator {
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+}
+</style>
---
+++ src/components/TextSelectOption.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title: string;
+  subtitle?: string;
+  accentColor?: string;
+}>();
+</script>
+
+<template>
+  <div class="text-select-option" :title="props.subtitle">
+    <div class="text-select-option-indicator" :style="{ backgroundColor: props.accentColor }"></div>
+    <div class="text-select-option-label">
+      {{ props.title }}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.text-select-option {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+}
+
+.text-select-option-indicator {
+  width: 16px;
+  height: 16px;
+  border-radius: 50%;
+}
+</style>
