1e27ede0b270340174d3f582bd2a2e6476ed160a
bkis <kiss@mailbox.org>
2023-04-19 16:45:33 +0200

    Update gunicorn config

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,5 +1,10 @@
+import multiprocessing
+
+
 bind = "0.0.0.0:8000"
-workers = 4
+workers = int(min(4, multiprocessing.cpu_count() * 2 + 1))
 worker_class = "uvicorn.workers.UvicornWorker"
 worker_tmp_dir = "/dev/shm"
+preload_app = True
+timeout = 30
 loglevel = "info"  # will be overridden by app config
