efc5f2b994c4f90828949d4a5076aabc34e9435e
bkis <kiss@mailbox.org>
2023-04-26 21:00:31 +0200

    Improve styling of secondary nav

--- src/components/navigation/SecondaryNavBar.vue
+++ src/components/navigation/SecondaryNavBar.vue
@@ -19,9 +19,16 @@
   padding: 0.3rem 0.5rem;
   border-radius: 4px;
   background-color: #80808030;
+  font-size: var(--app-ui-font-size-tiny);
+  border-bottom: 1px solid #80808060;
 }
 
 .secondary-navbar .navbar-router-link:hover {
   background-color: #80808050;
+  border-bottom: 1px solid #80808080;
+}
+
+.secondary-navbar .navbar-router-link.router-link-exact-active {
+  border-bottom: 1px solid var(--accent-color);
 }
 </style>
