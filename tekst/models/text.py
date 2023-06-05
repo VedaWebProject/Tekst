a4e736dff3f4a08494cf940e5dc0992056d767a6
bkis <kiss@mailbox.org>
2023-06-05 08:40:07 +0200

    Delete impossible default level validator

--- tekst/models/text.py
+++ tekst/models/text.py
@@ -82,15 +82,6 @@ class Text(ModelBase, ModelFactory):
             return None
         return v
 
-    @validator("default_level")
-    def validate_default_level(cls, v, values, **kwargs):
-        if v >= len(values["levels"]):
-            raise ValueError(
-                f"Invalid default level value ({v}). "
-                f"This text only has {len(values['levels'])} levels."
-            )
-        return v
-
     @validator("accent_color")
     def validate_color(cls, v) -> Color:
         if not isinstance(v, Color):
