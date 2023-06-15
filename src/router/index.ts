2f754ecf95077330e75c4ec739454847304b7b69
bkis <kiss@mailbox.org>
2023-06-15 11:05:01 +0200

    Remove references to deleted component

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,7 +15,6 @@ const UserView = () => import('@/views/UserView.vue');
 const HelpView = () => import('@/views/HelpView.vue');
 const BrowseView = () => import('@/views/BrowseView.vue');
 const SearchView = () => import('@/views/SearchView.vue');
-const LoginView = () => import('@/views/LoginView.vue');
 const RegisterView = () => import('@/views/RegisterView.vue');
 
 const AccountView = () => import('@/views/account/AccountView.vue');
@@ -66,11 +65,6 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: RegisterView,
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: LoginView,
     },
     {
       path: '/user/:username',
