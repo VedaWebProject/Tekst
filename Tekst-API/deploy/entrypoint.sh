85d216c2d8e5e9eacf3c8c5dca7029f3147c11da
bkis <kiss@mailbox.org>
2023-06-27 15:24:39 +0200

    Exit server startup on setup failure

--- Tekst-API/deploy/entrypoint.sh
+++ Tekst-API/deploy/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-python3 -m tekst setup
-exec gunicorn tekst.app:app --config gunicorn_conf.py "$@"
+python3 -m tekst setup && \
+exec gunicorn tekst.app:app --config gunicorn_conf.py "$@" || \
+exit 1
