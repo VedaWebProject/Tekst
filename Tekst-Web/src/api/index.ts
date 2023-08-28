5dcb5ca7b95b00b513cca2877a0aebf11f4c515f
bkis <kiss@mailbox.org>
2023-08-28 14:42:52 +0200

    Remove legacy code

--- Tekst-Web/src/api/index.ts
+++ Tekst-Web/src/api/index.ts
@@ -56,69 +56,6 @@ export const optionsPresets = {
   },
 };
 
-// // USE LIKE THIS:
-
-// <script setup lang="ts">
-// import { ref } from 'vue';
-// import { POST, GET, optionsPresets } from './api';
-// import type {components} from '@/api/types.d.ts'
-
-// type TextRead = components['schemas']['TextRead'];
-
-// const texts = ref<TextRead[]>([]);
-
-// async function handleClickLogin(){
-//   const { error } = await POST(
-//     '/auth/cookie/login',
-//     {
-//       body: {
-//         username: 'superuser@test.com',
-//         password: 'poiPOI098'
-//       },
-//       ...optionsPresets.formUrlEncoded
-//     });
-//   if (error) {
-//     console.error(`NOGOOD: ${error}`);
-//   } else {
-//     console.log("LOGGED IN!")
-//   }
-// }
-
-// async function handleClickTexts(){
-//   const { data, error } = await GET(
-//     '/texts',
-//     { params: {} });
-//   if (error) {
-//     console.error(error);
-//   } else {
-//     texts.value = data;
-//   }
-// }
-
-// async function handleClick401(){
-//   const { data, error } = await POST('/nodes', {body: {
-//     textId: '5eb7cf5a86d9755df3a6c593',
-//     level: 0,
-//     position: 0,
-//     label: 'Hello',
-//   }});
-//   if (error) {
-//     console.error(error);
-//   } else {
-//     console.log(`Created ${data}`)
-//   }
-// }
-// </script>
-
-// <template>
-//   <button type="button" @click="handleClickLogin">Login</button>
-//   <button type="button" @click="handleClickTexts">Texts</button>
-//   <button type="button" @click="handleClick401">401</button>
-//   <div v-for="text in texts" :key="text.id">
-//     {{ text.title }}
-//   </div>
-// </template>
-
 // export components types for use throughout codebase
 export type UserCreate = components['schemas']['UserCreate'];
 export type UserRead = components['schemas']['UserRead'];
