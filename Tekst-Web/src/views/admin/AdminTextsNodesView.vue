d57710766ac8117163333484674e695163f2eabd
bkis <kiss@mailbox.org>
2023-09-18 17:28:26 +0200

    Add admin text nodes view translations, warnings

--- Tekst-Web/src/views/admin/AdminTextsNodesView.vue
+++ Tekst-Web/src/views/admin/AdminTextsNodesView.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
-import { NButton, NIcon, NTree, useDialog, type TreeDropInfo, type TreeOption } from 'naive-ui';
+import {
+  NSpin,
+  NButton,
+  NIcon,
+  NTree,
+  NAlert,
+  NCheckbox,
+  useDialog,
+  type TreeDropInfo,
+  type TreeOption,
+} from 'naive-ui';
 import { h, ref } from 'vue';
 import { GET } from '@/api';
 import { useStateStore } from '@/stores';
@@ -11,7 +21,7 @@ import { watch } from 'vue';
 import type { Component } from 'vue';
 
 import ArrowForwardIosRound from '@vicons/material/ArrowForwardIosRound';
-import TagFacesOutlined from '@vicons/material/TagFacesOutlined';
+import EditTwotone from '@vicons/material/EditTwotone';
 
 const state = useStateStore();
 const { message } = useMessages();
@@ -20,6 +30,7 @@ const dialog = useDialog();
 
 const treeData = ref<TreeOption[]>([]);
 const selectedNode = ref<TreeOption | null>();
+const showWarnings = ref(true);
 
 async function handleLoad(node?: TreeOption) {
   const { data, error } = await GET('/nodes/children', {
@@ -85,7 +96,7 @@ function handleDeleteNode(node: TreeOption) {
 function renderSwitcherIcon() {
   return h(
     NIcon,
-    { size: 'var(--app-ui-font-size-medium)', style: 'position: relative; top: 4px' },
+    { size: 'var(--app-ui-font-size-medium)', style: 'align-self: center' },
     {
       default: () => h(ArrowForwardIosRound),
     }
@@ -124,10 +135,10 @@ function renderSuffixButton(icon: Component, onClick: () => void) {
 function renderSuffix(info: { option: TreeOption; checked: boolean; selected: boolean }) {
   if (!info.selected) return null;
   return h('div', { style: 'display: flex; gap: 4px; padding: 4px' }, [
-    renderSuffixButton(DeleteFilled, () => handleDeleteNode(info.option)),
-    renderSuffixButton(TagFacesOutlined, () => {
-      alert('something');
+    renderSuffixButton(EditTwotone, () => {
+      alert('EDIT!');
     }),
+    renderSuffixButton(DeleteFilled, () => handleDeleteNode(info.option)),
   ]);
 }
 
@@ -145,7 +156,17 @@ watch(
 <template>
   <h2>{{ $t('admin.texts.nodes.heading') }}</h2>
 
-  <div class="content-block">
+  <n-alert closable :title="$t('general.warning')" type="warning">
+    {{ $t('admin.texts.nodes.warnGeneral') }}
+  </n-alert>
+
+  <div style="padding: var(--layout-gap) 0 0 var(--content-gap)">
+    <n-checkbox v-model:checked="showWarnings">
+      {{ $t('admin.texts.nodes.checkShowWarnings') }}
+    </n-checkbox>
+  </div>
+
+  <div v-if="treeData.length" class="content-block">
     <n-tree
       block-line
       draggable
@@ -154,8 +175,18 @@ watch(
       :render-switcher-icon="renderSwitcherIcon"
       :render-label="renderLabel"
       :render-suffix="renderSuffix"
+      :expand-on-dragenter="false"
       @drop="handleDrop"
       @update:selected-keys="handleSelect"
     />
   </div>
+
+  <n-spin v-else style="margin: 3rem 0 2rem 0; width: 100%" :description="$t('init.loading')" />
 </template>
+
+<style>
+.n-tree-node-switcher,
+.n-tree-node-switcher--expanded {
+  align-self: center;
+}
+</style>
