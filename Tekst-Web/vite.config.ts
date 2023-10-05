f8843f6548bbf092c2e231787d71c25d2852a0b7
bkis <kiss@mailbox.org>
2023-10-05 14:32:55 +0200

    Optimize client build config

--- Tekst-Web/vite.config.ts
+++ Tekst-Web/vite.config.ts
@@ -8,6 +8,11 @@ import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  envPrefix: 'TEKST_',
+  appType: 'spa',
+  server: {
+    host: '127.0.0.1',
+  },
   plugins: [
     vue(),
     checker({
@@ -27,9 +32,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  envPrefix: 'TEKST_',
-  appType: 'spa',
-  server: {
-    host: '127.0.0.1',
+  define: {
+    __VUE_I18N_FULL_INSTALL__: false,
+    __VUE_I18N_LEGACY_API__: false,
+    __INTLIFY_PROD_DEVTOOLS__: false,
   },
 });
