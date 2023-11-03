f547a93e626555bc29facc5affbcb0f59a9a4600
bkis <kiss@mailbox.org>
2023-11-03 11:15:36 +0100

    Add page segments cache in pf data composable

--- Tekst-Web/src/platformData.ts
+++ Tekst-Web/src/platformData.ts
@@ -1,8 +1,9 @@
 import { ref } from 'vue';
 import { GET } from '@/api';
-import type { PlatformData } from '@/api';
+import type { ClientSegmentRead, PlatformData } from '@/api';
 
 const data = ref<PlatformData>();
+const pageSegments = ref<ClientSegmentRead[]>([]);
 
 export function usePlatformData() {
   async function loadPlatformData() {
@@ -14,5 +15,31 @@ export function usePlatformData() {
       throw error;
     }
   }
-  return { pfData: data, loadPlatformData };
+
+  async function getSegment(key?: string, locale?: string) {
+    if (!key) return null;
+    if (key.startsWith('system_')) {
+      const segments = data.value?.systemSegments.filter((s) => s.key === key) || [];
+      return (
+        segments.find((s) => s.locale === locale) ||
+        segments.find((s) => s.locale === 'enUS') ||
+        segments[0] ||
+        null
+      );
+    } else {
+      const segment = pageSegments.value.find((s) => s.key === key && s.locale === locale);
+      if (segment) return segment;
+      const { data, error } = await GET('/platform/segments', {
+        params: { query: { key, ...(locale ? { locale } : {}) } },
+      });
+      if (!error) {
+        pageSegments.value.push(data);
+        return data;
+      } else {
+        console.error(`Error loading segment ${key}`, error);
+      }
+    }
+  }
+
+  return { pfData: data, loadPlatformData, getSegment };
 }
