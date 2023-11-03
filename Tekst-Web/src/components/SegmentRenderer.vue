1f1047902edcc95fc1b27757f45d8a80c6321329
bkis <kiss@mailbox.org>
2023-11-03 11:13:11 +0100

    Implement SegmentRenderer component

---
+++ Tekst-Web/src/components/SegmentRenderer.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { usePlatformData } from '@/platformData';
+import { useStateStore } from '@/stores';
+import { watchEffect, ref } from 'vue';
+
+const props = defineProps<{
+  segmentKey: string;
+}>();
+
+const { getSegment } = usePlatformData();
+const state = useStateStore();
+
+const html = ref<string>();
+
+watchEffect(async () => {
+  html.value = (await getSegment(props.segmentKey, state.locale))?.html;
+});
+</script>
+
+<template>
+  <!-- eslint-disable-next-line vue/no-v-html -->
+  <div v-if="html" class="segment-container" v-html="html"></div>
+</template>
+
+<style>
+.segment-container > p:only-child {
+  margin: 0;
+}
+</style>
