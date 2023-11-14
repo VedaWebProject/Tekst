5bc3039a6409205151cca8df486723804f85e8df
bkis <kiss@mailbox.org>
2023-11-14 16:12:21 +0100

    Remove old dummy translations

--- Tekst-Web/src/views/HomeView.vue
+++ Tekst-Web/src/views/HomeView.vue
@@ -11,27 +11,27 @@ const systemHomeExists = computed(
 
 <template>
   <PageView v-if="systemHomeExists" page-key="systemHome" />
-  <div v-else class="content-block">
-    <h1>{{ pfData?.settings.infoPlatformName }} ({{ $t('dummy.home') }})</h1>
-    <p>{{ $t('dummy.schiller') }}</p>
+  <template v-else>
+    <h1>{{ pfData?.settings.infoPlatformName }}</h1>
+    <div class="content-block">
+      <h2>Accent Palette Demo</h2>
+      <div class="square" style="background-color: #305d97"></div>
+      <div class="square" style="background-color: #097f86"></div>
+      <div class="square" style="background-color: #43895f"></div>
+      <div class="square" style="background-color: #d49101"></div>
+      <div class="square" style="background-color: #d26e2b"></div>
+      <div class="square" style="background-color: #d43a35"></div>
+      <div class="square" style="background-color: #b83e63"></div>
+      <div class="square" style="background-color: #88447f"></div>
 
-    <h2>Accent Palette Demo</h2>
-    <div class="square" style="background-color: #305d97"></div>
-    <div class="square" style="background-color: #097f86"></div>
-    <div class="square" style="background-color: #43895f"></div>
-    <div class="square" style="background-color: #d49101"></div>
-    <div class="square" style="background-color: #d26e2b"></div>
-    <div class="square" style="background-color: #d43a35"></div>
-    <div class="square" style="background-color: #b83e63"></div>
-    <div class="square" style="background-color: #88447f"></div>
-
-    <h2>Current Accent Color Demo</h2>
-    <div class="square" style="background-color: var(--accent-color)"></div>
-    <div class="square" style="background-color: var(--accent-color-fade1)"></div>
-    <div class="square" style="background-color: var(--accent-color-fade2)"></div>
-    <div class="square" style="background-color: var(--accent-color-fade3)"></div>
-    <div class="square" style="background-color: var(--accent-color-fade4)"></div>
-  </div>
+      <h2>Current Accent Color Demo</h2>
+      <div class="square" style="background-color: var(--accent-color)"></div>
+      <div class="square" style="background-color: var(--accent-color-fade1)"></div>
+      <div class="square" style="background-color: var(--accent-color-fade2)"></div>
+      <div class="square" style="background-color: var(--accent-color-fade3)"></div>
+      <div class="square" style="background-color: var(--accent-color-fade4)"></div>
+    </div>
+  </template>
 </template>
 
 <style scoped>
