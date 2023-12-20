8a803593038527dd4f082f691b9205242500f32e
bkis <kiss@mailbox.org>
2023-12-20 16:48:24 +0100

    Remove old temporary whitespace

--- Tekst-Web/src/views/BrowseView.vue
+++ Tekst-Web/src/views/BrowseView.vue
@@ -35,7 +35,7 @@ const activeResourcesCategorized = computed(() =>
 
 <template>
   <IconHeading level="1" :icon="MenuBookOutlined">
-    <LocationLabel />&nbsp;
+    <LocationLabel />
     <HelpButtonWidget help-key="browseView" />
   </IconHeading>
 
