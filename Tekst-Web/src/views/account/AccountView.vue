f3fbaeba0f5cb656ebfe1f6029e434af665fe2aa
bkis <kiss@mailbox.org>
2024-01-02 10:11:51 +0100

    Fix account view navigation alignment

--- Tekst-Web/src/views/account/AccountView.vue
+++ Tekst-Web/src/views/account/AccountView.vue
@@ -1,17 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import NavigationMenu from '@/components/navigation/NavigationMenu.vue';
-import { useStateStore } from '@/stores';
 import { useAccountMenuOptions } from '@/components/navigation/navMenuOptions';
 
-const state = useStateStore();
 const { menuOptions } = useAccountMenuOptions();
 </script>
 
 <template>
-  <div v-if="!state.smallScreen" style="display: flex; justify-content: center">
-    <NavigationMenu :options="menuOptions" embed />
-  </div>
+  <NavigationMenu :options="menuOptions" embed center />
   <router-view></router-view>
 </template>
 
