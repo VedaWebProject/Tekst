f7e03392bc4b82c40225b870b0e00a976bff19c7
bkis <kiss@mailbox.org>
2024-01-04 11:44:50 +0100

    Make user prop optional for user display component

--- Tekst-Web/src/components/UserDisplay.vue
+++ Tekst-Web/src/components/UserDisplay.vue
@@ -8,11 +8,12 @@ import PersonFilled from '@vicons/material/PersonFilled';
 
 withDefaults(
   defineProps<{
-    user: UserReadPublic & Record<string, any>;
+    user?: UserReadPublic & Record<string, any>;
     showIcon?: boolean;
     size?: 'large' | 'medium' | 'small' | 'tiny' | 'mini';
   }>(),
   {
+    user: undefined,
     showIcon: true,
     size: undefined,
   }
@@ -21,13 +22,13 @@ withDefaults(
 
 <template>
   <div
-    v-if="user"
     style="display: flex; align-items: center"
     :style="size ? `font-size: var(--app-ui-font-size-${size})` : ''"
   >
     <n-icon v-if="showIcon" :component="PersonFilled" style="margin-right: 0.25rem" />
-    <RouterLink :to="{ name: 'user', params: { username: user.username } }">
+    <RouterLink v-if="user" :to="{ name: 'user', params: { username: user.username } }">
       <UserDisplayText :user="user" />
     </RouterLink>
+    <span v-else>–</span>
   </div>
 </template>
