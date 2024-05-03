6156394f9745d9f1f724583d0b21c11732dbc31a
bkis <kiss@mailbox.org>
2024-05-03 16:19:23 +0200

    Update incompatible test cases

--- Tekst-API/tests/test_setup.py
+++ Tekst-API/tests/test_setup.py
@@ -4,11 +4,11 @@ from tekst.setup import app_setup
 
 
 @pytest.mark.anyio
-async def test_setup(config):
-    await app_setup(config)
+async def test_setup():
+    await app_setup()
 
 
 @pytest.mark.anyio
-async def test_setup_db_has_data(config, insert_sample_data):
+async def test_setup_db_has_data(insert_sample_data):
     await insert_sample_data("texts", "locations", "resources")
-    await app_setup(config)
+    await app_setup()
