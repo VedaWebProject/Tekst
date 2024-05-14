f88279be9ca0ec0bdcb338c37e6abb5e78d36f56
bkis <kiss@mailbox.org>
2024-05-14 14:03:39 +0200

    Clean up and fix string utils, adjust test cases

--- Tekst-API/tekst/utils/strings.py
+++ Tekst-API/tekst/utils/strings.py
@@ -5,8 +5,10 @@ def cleanup_spaces_multiline(string: str | None) -> str | None:
     """Reduces excessive newline chars and whitespaces and strips whitespaces"""
     if string is None:
         return None
-    string = re.sub(r"[\n\r]{3,}", "\n\n", string)
-    string = re.sub(r"[\t ]+", " ", string)
+    string = re.sub(r"(\r\n|\r|\n)", "\n", string)  # normalize line breaks
+    string = re.sub(r"[\t ]*\n[\t ]*", "\n", string)  # remove spaces around line breaks
+    string = re.sub(r"\n{3,}", "\n\n", string)  # max. 2 consecutive line breaks
+    string = re.sub(r"[\t ]+", " ", string)  # replace excessive whitespaces
     return string.strip()
 
 
@@ -14,4 +16,6 @@ def cleanup_spaces_oneline(string: str | None) -> str | None:
     """Replaces any number of newlines with one whitespace and strips whitespaces"""
     if string is None:
         return None
-    return re.sub(r"[\n\r\t ]+", " ", string).strip()
+    return re.sub(
+        r"\s+", " ", string
+    ).strip()  # replace any sequence of spaces with one whitespace
--- Tekst-API/tests/test_utils_strings.py
+++ Tekst-API/tests/test_utils_strings.py
@@ -3,9 +3,13 @@ from tekst.utils import strings
 
 def test_cleanup_spaces_multiline():
     assert (
-        strings.cleanup_spaces_multiline(" Foo  Bar\n\n\nBaz\n\n\n") == "Foo Bar\n\nBaz"
+        strings.cleanup_spaces_multiline(" Foo \n \n\t\n Bar  Bar\n\n\nBaz\n\n\n")
+        == "Foo\n\nBar Bar\n\nBaz"
     )
 
 
 def test_cleanup_spaces_oneline():
-    assert strings.cleanup_spaces_oneline(" Foo  Bar\n\n\nBaz\n\n\n") == "Foo Bar Baz"
+    assert (
+        strings.cleanup_spaces_oneline(" Foo \n \n\t\n Bar  Bar\n\n\nBaz\n\n\n")
+        == "Foo Bar Bar Baz"
+    )
