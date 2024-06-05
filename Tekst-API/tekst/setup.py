5078897ca414b1a816bea3bd50a5cd89417406ac
bkis <kiss@mailbox.org>
2024-06-05 16:09:53 +0200

    Only setup ES under certain conditions

--- Tekst-API/tekst/setup.py
+++ Tekst-API/tekst/setup.py
@@ -1,5 +1,6 @@
 from tekst import db
 from tekst.auth import create_initial_superuser
+from tekst.config import TekstConfig, get_config
 from tekst.logging import log, setup_logging
 from tekst.resources import init_resource_types_mgr
 from tekst.sample_data import insert_sample_data
@@ -7,12 +8,14 @@ from tekst.search import setup_elasticsearch, task_create_index
 
 
 async def app_setup():
+    cfg: TekstConfig = get_config()
     setup_logging()
     log.info("Running Tekst pre-launch app setup...")
     init_resource_types_mgr()
     await db.init_odm()
     await insert_sample_data()
     await create_initial_superuser()  # happens only when not in DEV mode
-    await setup_elasticsearch()
-    await task_create_index()
+    if not cfg.dev_mode or cfg.dev_use_es:
+        await setup_elasticsearch()
+        await task_create_index()
     log.info("Finished Tekst pre-launch app setup.")
