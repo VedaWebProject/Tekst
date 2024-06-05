baeb116923823b06b1185c7648a4845e1129a7e9
bkis <kiss@mailbox.org>
2024-06-05 14:56:43 +0200

    Increate gunicorn worker timeout
    ...because the workers have to generate export data, create the search index, etc... they will need their time for heavy tasks

--- Tekst-API/deployment/gunicorn/gunicorn_conf.py
+++ Tekst-API/deployment/gunicorn/gunicorn_conf.py
@@ -6,5 +6,5 @@ workers = int(min(4, multiprocessing.cpu_count() * 2 + 1))
 worker_class = "uvicorn.workers.UvicornWorker"
 worker_tmp_dir = "/dev/shm"
 preload_app = True
-timeout = 30
+timeout = 240  # this has to be quite long as the workers also generate export data etc.
 loglevel = "info"  # will be overridden by app config
