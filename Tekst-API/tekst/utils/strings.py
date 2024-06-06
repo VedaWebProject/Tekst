74ace0f7d25b8f426e79c8606f356143578a6f78
bkis <kiss@mailbox.org>
2024-06-06 13:45:28 +0200

    Fix string cleanup validator functions for non-string input

--- Tekst-API/tekst/utils/strings.py
+++ Tekst-API/tekst/utils/strings.py
@@ -5,6 +5,7 @@ def cleanup_spaces_multiline(string: str | None) -> str | None:
     """Reduces excessive newline chars and whitespaces and strips whitespaces"""
     if string is None:
         return None
+    string = str(string)
     string = re.sub(r"(\r\n|\r|\n)", "\n", string)  # normalize line breaks
     string = re.sub(r"[\t ]*\n[\t ]*", "\n", string)  # remove spaces around line breaks
     string = re.sub(r"\n{3,}", "\n\n", string)  # max. 2 consecutive line breaks
@@ -16,6 +17,5 @@ def cleanup_spaces_oneline(string: str | None) -> str | None:
     """Replaces any number of newlines with one whitespace and strips whitespaces"""
     if string is None:
         return None
-    return re.sub(
-        r"\s+", " ", string
-    ).strip()  # replace any sequence of spaces with one whitespace
+    # replace any sequence of spaces with one whitespace, return the result
+    return re.sub(r"\s+", " ", str(string)).strip()
