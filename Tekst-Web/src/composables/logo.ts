231e68a87a758b219ced626f32a905845e787ad6
bkis <kiss@mailbox.org>
2024-06-14 10:56:43 +0200

    Fix logos composable

--- Tekst-Web/src/composables/logo.ts
+++ Tekst-Web/src/composables/logo.ts
@@ -2,24 +2,24 @@ import { STATIC_PATH } from '@/common';
 import { useFetch, usePreferredDark } from '@vueuse/core';
 import logo from '@/assets/logo.png';
 import logoDarkmode from '@/assets/logo-darkmode.png';
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { useThemeStore } from '@/stores';
 
 const customLogo = `${STATIC_PATH}/logo.png`;
 const customLogoDarkmode = `${STATIC_PATH}/logo-darkmode.png`;
-const { isFetching: fetchingCustomLogo, error: customLogoError } = await useFetch(customLogo);
-const { isFetching: fetchingCustomDarkLogo, error: customDarkLogoError } =
-  await useFetch(customLogoDarkmode);
+const customLogoAvailable = ref(false);
+const customLogoDarkmodeAvailable = ref(false);
+
+(async () => {
+  customLogoAvailable.value = await useFetch(customLogo).error.value;
+  customLogoDarkmodeAvailable.value = await useFetch(customLogoDarkmode).error.value;
+})();
 
 export function useLogo() {
   const theme = useThemeStore();
   const darkPreferred = usePreferredDark();
-  const logoLight = computed(() =>
-    customLogoError.value || fetchingCustomLogo.value ? logo : customLogo
-  );
-  const logoDark = computed(() =>
-    customDarkLogoError.value || fetchingCustomDarkLogo.value ? logoDarkmode : customLogoDarkmode
-  );
+  const logoLight = computed(() => (!customLogoAvailable.value ? logo : customLogo));
+  const logoDark = computed(() => (!customLogoAvailable.value ? logoDarkmode : customLogoDarkmode));
   const pageLogo = computed(() => (theme.darkMode ? logoDark.value : logoLight.value));
   const favicon = computed(() => (darkPreferred.value ? logoDark.value : logoLight.value));
   return { pageLogo, favicon };
