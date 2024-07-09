0b32f6a4e6765eaf99b7551a9fe39701a4114b72
bkis <kiss@mailbox.org>
2024-07-09 11:46:59 +0200

    Fix: Choice of displayed favicon is not deterministic
    Possible fix. Let's see...
    
    Closes #270

--- Tekst-Web/src/composables/logo.ts
+++ Tekst-Web/src/composables/logo.ts
@@ -6,22 +6,18 @@ import { computed, ref } from 'vue';
 import { useThemeStore } from '@/stores';
 
 const customLogo = `${STATIC_PATH}/logo.png`;
-const customLogoDarkmode = `${STATIC_PATH}/logo-darkmode.png`;
-const customLogoAvailable = ref(false);
-const customLogoDarkmodeAvailable = ref(false);
+const customLogoDark = `${STATIC_PATH}/logo-darkmode.png`;
+const logoLight = ref<string>();
+const logoDark = ref<string>();
 
 (async () => {
-  customLogoAvailable.value = !(await useFetch(customLogo)).error.value;
-  customLogoDarkmodeAvailable.value = !(await useFetch(customLogoDarkmode)).error.value;
+  logoLight.value = !(await useFetch(customLogo)).error.value ? customLogo : logo;
+  logoDark.value = !(await useFetch(customLogoDark)).error.value ? customLogoDark : logoDarkmode;
 })();
 
 export function useLogo() {
   const theme = useThemeStore();
   const darkPreferred = usePreferredDark();
-  const logoLight = computed(() => (customLogoAvailable.value ? customLogo : logo));
-  const logoDark = computed(() =>
-    customLogoDarkmodeAvailable.value ? customLogoDarkmode : logoDarkmode
-  );
   const pageLogo = computed(() => (theme.darkMode ? logoDark.value : logoLight.value));
   const favicon = computed(() => (darkPreferred.value ? logoDark.value : logoLight.value));
   return { pageLogo, favicon };
