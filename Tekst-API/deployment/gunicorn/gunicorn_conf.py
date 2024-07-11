b65344303b17a91eaff4d5d772bf2bfc18120115
bkis <kiss@mailbox.org>
2024-07-11 14:13:49 +0200

    Increase preset gunicorn worker timeout

--- Tekst-API/deployment/gunicorn/gunicorn_conf.py
+++ Tekst-API/deployment/gunicorn/gunicorn_conf.py
@@ -4,7 +4,7 @@ import os
 
 bind = "0.0.0.0:8000"
 preload_app = True
-timeout = 240  # this has to be quite long as the workers also generate export data etc.
+timeout = 480  # this has to be quite long as the workers also generate export data etc.
 
 workers = int(min(4, multiprocessing.cpu_count() * 2 + 1))
 worker_class = "uvicorn.workers.UvicornWorker"
