599e1c9b1c91b537b1ce37798092993ef70dfa02
bkis <kiss@mailbox.org>
2024-10-02 14:31:19 +0200

    Use scroll component w/ max height for error message details
    Closes #381

--- Tekst-Web/src/components/messages/GlobalMessageContent.vue
+++ Tekst-Web/src/components/messages/GlobalMessageContent.vue
@@ -1,19 +1,32 @@
 <script setup lang="ts">
-import { NCollapse, NCollapseItem } from 'naive-ui';
+import { NScrollbar, NCollapse, NCollapseItem } from 'naive-ui';
 </script>
 
 <template>
   <slot></slot>
   <n-collapse v-if="$slots.details" class="mt-md">
     <n-collapse-item :title="$t('general.details')" name="details" class="text-mini m-0">
-      <pre
-        style="
-          font-family: monospace;
-          font-size: var(--font-size-mini);
-          white-space: pre-wrap;
-          line-height: 1;
-        "
-      ><slot name="details"></slot></pre>
+      <n-scrollbar class="details-scroll-container">
+        <pre class="details">
+          <slot name="details"></slot>
+        </pre>
+      </n-scrollbar>
     </n-collapse-item>
   </n-collapse>
 </template>
+
+<style scoped>
+.details {
+  font-family: monospace;
+  font-size: var(--font-size-mini);
+  white-space: pre-wrap;
+  line-height: 1;
+}
+
+:deep(.n-scrollbar.details-scroll-container) {
+  max-height: 120px;
+  background-color: #00000010;
+  border-radius: 3px;
+  padding: 0.5rem;
+}
+</style>
