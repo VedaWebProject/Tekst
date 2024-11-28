09b89b6784fc495a3913f0aeec18043e06491e8e
bkis <kiss@mailbox.org>
2024-11-28 11:38:33 +0100

    Use vite-bundle-visualizer

--- Tekst-Web/vite.config.ts
+++ Tekst-Web/vite.config.ts
@@ -3,8 +3,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
 import vue from '@vitejs/plugin-vue';
-import { visualizer } from 'rollup-plugin-visualizer';
-import { defineConfig, PluginOption } from 'vite';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,7 +17,6 @@ export default defineConfig({
     VueI18nPlugin({
       include: resolve(dirname(fileURLToPath(import.meta.url)), './translations/ui/**'),
     }),
-    visualizer() as PluginOption,
   ],
   resolve: {
     alias: {
