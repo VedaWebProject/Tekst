1d3c1c3093ee5bfa33def3fb0e091ef47fab3db2
bkis <kiss@mailbox.org>
2025-05-21 12:21:57 +0200

    Fix migration code coverage exclusion

--- Tekst-API/tekst/db/migrations/__init__.py
+++ Tekst-API/tekst/db/migrations/__init__.py
@@ -19,7 +19,7 @@ def _sort_migrations(migrations: MigrationsDict) -> MigrationsDict:
 def _all_migrations() -> MigrationsDict:
     all_migrations = dict()
     for mig_mod in iter_modules(__path__):
-        if not mig_mod.name.startswith("migration_"):
+        if not mig_mod.name.startswith("migration_"):  # pragma: no cover
             continue
         mig_name = mig_mod.name.replace("migration_", "").replace("_", ".")
         mig_fn = import_module(f"{__name__}.{mig_mod.name}").migration
