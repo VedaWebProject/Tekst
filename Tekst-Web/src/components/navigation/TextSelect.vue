fad65713a4a85654b61d7ff64c3b0d5769fdf298
bkis <kiss@mailbox.org>
2025-05-30 10:01:11 +0200

    Remove superfluous text info button
    Closes #989

--- Tekst-Web/src/components/navigation/TextSelect.vue
+++ Tekst-Web/src/components/navigation/TextSelect.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
 import type { TextRead } from '@/api';
-import GenericModal from '@/components/generic/GenericModal.vue';
 import TextSelectOption from '@/components/navigation/TextSelectOption.vue';
-import { ExpandArrowDownIcon, InfoIcon, ResourceIcon, TextsIcon } from '@/icons';
-import { useBrowseStore, useResourcesStore, useStateStore } from '@/stores';
-import { pickTranslation } from '@/utils';
-import { NButton, NDropdown, NFlex, NIcon, NTag, useThemeVars } from 'naive-ui';
+import { ExpandArrowDownIcon } from '@/icons';
+import { useBrowseStore, useStateStore } from '@/stores';
+import { NButton, NDropdown, NFlex, NIcon, useThemeVars } from 'naive-ui';
 import { computed, h, ref } from 'vue';
 import { useRouter } from 'vue-router';
 
@@ -13,15 +11,9 @@ const router = useRouter();
 const state = useStateStore();
 const browse = useBrowseStore();
 const themeVars = useThemeVars();
-const resources = useResourcesStore();
 
 const disabled = computed(() => !state.pf?.texts || state.pf.texts.length <= 1);
 const dropdownRef = ref();
-const showInfoModal = ref(false);
-
-const resourcesByLevel = computed(() =>
-  state.textLevelLabels.map((_, i) => resources.ofText.filter((r) => r.level === i))
-);
 
 const renderLabel = (t: TextRead) => {
   return () =>
@@ -64,86 +56,33 @@ function handleSelect(text: TextRead) {
 </script>
 
 <template>
-  <n-flex align="center" :wrap="false">
-    <n-dropdown
-      v-if="state.text"
-      ref="dropdownRef"
-      trigger="click"
-      :options="options"
-      :disabled="disabled"
-      placement="bottom-start"
-    >
-      <n-button
-        text
-        icon-placement="right"
-        :color="themeVars.baseColor"
-        :focusable="false"
-        :keyboard="false"
-        :title="$t('common.textSelect')"
-        class="text-select-btn"
-        :style="{ cursor: !disabled ? 'pointer' : 'default' }"
-      >
-        <n-flex align="center" :wrap="false" style="max-width: 100%">
-          <span class="text-title ellipsis text-large">{{ state.text.title }}</span>
-          <n-icon v-if="!disabled" :component="ExpandArrowDownIcon" style="flex-shrink: 0" />
-        </n-flex>
-      </n-button>
-    </n-dropdown>
-
+  <n-dropdown
+    v-if="state.text"
+    ref="dropdownRef"
+    trigger="click"
+    :options="options"
+    :disabled="disabled"
+    placement="bottom-start"
+  >
     <n-button
-      v-if="!state.smallScreen"
-      quaternary
-      circle
-      color="var(--base-color)"
+      text
+      icon-placement="right"
+      :color="themeVars.baseColor"
       :focusable="false"
       :keyboard="false"
-      :title="$t('common.information')"
-      class="text-info-btn"
-      @click="showInfoModal = true"
+      :title="$t('common.textSelect')"
+      class="text-select-btn"
+      :style="{ cursor: !disabled ? 'pointer' : 'default' }"
     >
-      <template #icon>
-        <n-icon :component="InfoIcon" />
-      </template>
+      <n-flex align="center" :wrap="false" style="max-width: 100%">
+        <span class="text-title ellipsis text-large">{{ state.text.title }}</span>
+        <n-icon v-if="!disabled" :component="ExpandArrowDownIcon" style="flex-shrink: 0" />
+      </n-flex>
     </n-button>
-
-    <generic-modal
-      v-model:show="showInfoModal"
-      :title="state.text?.title || $t('common.information')"
-      :icon="TextsIcon"
-    >
-      <h3>{{ $t('models.resource.modelLabel', 2) }} / {{ $t('common.level') }}</h3>
-      <template
-        v-for="(levelLabel, levelIndex) in state.textLevelLabels"
-        :key="`level-${levelIndex}`"
-      >
-        <h4>{{ levelLabel }}</h4>
-        <ul v-if="!!resourcesByLevel[levelIndex].length">
-          <li v-for="res in resourcesByLevel[levelIndex]" :key="res.id">
-            <n-flex align="center">
-              <span>{{ pickTranslation(res.title, state.locale) }}</span>
-              <n-tag size="small">
-                <template #icon>
-                  <n-icon :component="ResourceIcon" />
-                </template>
-                {{ $t(`resources.types.${res.resourceType}.label`) }}
-              </n-tag>
-            </n-flex>
-          </li>
-        </ul>
-        <p v-else class="text-medium translucent i">
-          {{ $t('texts.levels.noResources') }}
-        </p>
-      </template>
-    </generic-modal>
-  </n-flex>
+  </n-dropdown>
 </template>
 
 <style scoped>
-ul {
-  margin-top: 0;
-  padding-left: var(--gap-lg);
-}
-
 .text-select-btn {
   max-width: 100%;
   justify-content: flex-start;
