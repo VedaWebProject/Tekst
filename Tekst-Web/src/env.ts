44c6a2aaea633ee7755f11ca3c4bcbeba6b5a5e1
bkis <kiss@mailbox.org>
2025-06-04 10:43:54 +0200

    Document why client assumes API path with web path prefix by default

--- Tekst-Web/src/env.ts
+++ Tekst-Web/src/env.ts
@@ -8,6 +8,11 @@ class Environment {
     this.WEB_PATH = baseHref ? new URL(baseHref).pathname : import.meta.env.BASE_URL || '/';
     const webPathStripped = this.WEB_PATH.replace(/\/+$/, '');
     this.STATIC_PATH = webPathStripped + '/static';
+    // construct API path: If the API path is set via an env var, it's assumed to
+    // contain the COMPLETE path to the API (including the web path
+    // or any other path prefix).
+    // If not, we assume that we're running the official container image and
+    // the API is located under the web path.
     this.TEKST_API_PATH = import.meta.env.TEKST_API_PATH || webPathStripped + '/api';
   }
 }
