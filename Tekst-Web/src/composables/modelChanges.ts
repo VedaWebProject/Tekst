b63aa47790ec5cfb8b1df142d05c57e794e58dec
Börge Kiss <kiss@mailbox.org>
2026-02-09 13:30:54 +0100

    Ask for confirmation when navigation away from unsaved dirty forms Fixes #1541

--- Tekst-Web/src/composables/modelChanges.ts
+++ Tekst-Web/src/composables/modelChanges.ts
@@ -1,12 +1,35 @@
+import { $t } from '@/i18n';
+import { useDialog } from 'naive-ui';
 import { computed, ref, type Ref } from 'vue';
+import { onBeforeRouteLeave } from 'vue-router';
 
 export function useModelChanges(model: Ref<Record<string, unknown> | undefined>) {
+  const dialog = useDialog();
   const valuesToJSON = (o: Record<string, unknown> | undefined) =>
     Object.fromEntries(Object.entries(o || {}).map(([k, v]) => [k, JSON.stringify(v)]));
   const beforeEntriesJson = ref(valuesToJSON(model.value));
   const afterEntriesJson = computed(() => valuesToJSON(model.value));
   const changed = computed(() =>
     Object.entries(afterEntriesJson.value).some(([k, v]) => v !== beforeEntriesJson.value[k])
+  );
+
+  // register router guard to prevent navigation if there are unsaved changes
+  onBeforeRouteLeave(
+    async (_to, _from) =>
+      !changed.value ||
+      (await new Promise((resolve) =>
+        dialog.warning({
+          title: $t('common.warning'),
+          content: $t('common.dirtyFormConfirm'),
+          positiveText: $t('common.yes'),
+          negativeText: $t('common.no'),
+          onPositiveClick: () => resolve(true),
+          onNegativeClick: () => resolve(false),
+          onClose: () => resolve(false),
+          onEsc: () => resolve(false),
+          onMaskClick: () => resolve(false),
+        })
+      ))
   );
 
   function getChanges(forceProps?: string[]) {
